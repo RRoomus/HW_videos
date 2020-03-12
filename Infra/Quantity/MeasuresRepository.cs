306d13b3e7f904454e32789483ccbd6f5174deab
Reia <reroom@ttu.ee>
2020-03-12 18:26:44 +0200

    26 video lõpp

--- Infra/Quantity/MeasuresRepository.cs
+++ Infra/Quantity/MeasuresRepository.cs
@@ -10,17 +10,5 @@ namespace Abc.Infra.Quantity
         public MeasuresRepository(QuantityDbContext c) : base(c, c.Measures) { }
 
         protected internal override Measure toDomainObject(MeasureData d) => new Measure(d);
-
-        protected internal override IQueryable<MeasureData> addFiltering(IQueryable<MeasureData> set)
-        {
-            if (string.IsNullOrEmpty(SearchString)) return set;
-            return set.Where(s => s.Name.Contains(SearchString)
-                        || s.Code.Contains(SearchString)
-                        || s.Id.Contains(SearchString)
-                        || s.Definition.Contains(SearchString)
-                        || s.ValidFrom.ToString().Contains(SearchString)
-                        || s.ValidTo.ToString().Contains(SearchString)
-                );
-        }
     }
 }
--- Infra/Quantity/QuantityDbInitializer.cs
+++ Infra/Quantity/QuantityDbInitializer.cs
@@ -10,36 +10,36 @@ namespace Abc.Infra.Quantity
         internal static List<MeasureData> measures => new List<MeasureData>
         {
             new MeasureData {Id = "Time", Name = "Time", Code = "t",
-            Definition = "In physical science, time is defined as a measurement, "+
+            Definition = "In physical science, time is defined as a measurement, " +
                 "or as what the clock face reads."},
             new MeasureData {Id = "Length", Name = "Length", Code = "l",
             Definition = "The measurement or extent of something from end to end."},
             new MeasureData {Id = "Mass", Name = "Mass", Code = "m",
-            Definition = "The quantity of matter wich a body contains, as measured by "+
-                "its acceleration under a given force or by the force exerted on "+
+            Definition = "The quantity of matter wich a body contains, as measured by " +
+                "its acceleration under a given force or by the force exerted on " +
                 "it by a gravitational field."},
             new MeasureData {Id = "Current", Name = "Electric Current", Code = "I",
-            Definition = "An eletric current is the rate of flow of electric charge "+
-                "past a point or region. An electric current is said to exist "+
-                "when there is a net flow of electric charge through a region. "+
-                "In electric circruits this charge is often carried by electrons "+
-                "moving through a wire. It can also be carried by ions in an "+
-                "electrolyte, or by both ions and electrons such as in an "+
+            Definition = "An eletric current is the rate of flow of electric charge " +
+                "past a point or region. An electric current is said to exist " +
+                "when there is a net flow of electric charge through a region. " +
+                "In electric circruits this charge is often carried by electrons " +
+                "moving through a wire. It can also be carried by ions in an " +
+                "electrolyte, or by both ions and electrons such as in an " +
                 "ionized gas (plasma)."},
             new MeasureData {Id = "Temperature", Name = "Thermodynamic Temperature", Code = "T",
-            Definition = "Thermodynamic temperature is the absolute measure of temperature "+
+            Definition = "Thermodynamic temperature is the absolute measure of temperature " +
                 "and is one of the principal parameters of thermodynamics. "},
             new MeasureData {Id = "Substance", Name = "Amount of Substance", Code = "n",
-            Definition = "In chemistry, the amount of substance in a given "+
-                "sample of matter is the number of discrete atomic-scale "+
-                "particles in it; where the particles may be molecules, "+
-                "atoms, ions, electrons, or other, depending on the context. "+
+            Definition = "In chemistry, the amount of substance in a given " +
+                "sample of matter is the number of discrete atomic-scale " +
+                "particles in it; where the particles may be molecules, " +
+                "atoms, ions, electrons, or other, depending on the context. " +
                 "It is sometimes referred to as the chemical amount. "},
             new MeasureData {Id = "Luminous", Name = "Luminous Intensity", Code = "Iv",
-            Definition = "In photometry, luminous intensity is a measure of the "+
-                "wavelenght-weigted powder emitted by a light source in a "+
-                "particular direction per unit solid angle, based on the "+
-                "luminosity function, a standardized model of the sensitivity "+
+            Definition = "In photometry, luminous intensity is a measure of the " +
+                "wavelenght-weigted powder emitted by a light source in a " +
+                "particular direction per unit solid angle, based on the " +
+                "luminosity function, a standardized model of the sensitivity " +
                 "of the human eye. "}
         };
 
