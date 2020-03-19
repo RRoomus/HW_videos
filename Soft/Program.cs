caaca8be3c1a48a19b2a2b0eea9b526c92be5b80
Reia <reroom@ttu.ee>
2020-03-19 16:06:56 +0200

    35 video lõpp

--- Soft/Program.cs
+++ Soft/Program.cs
@@ -1,14 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Abc.Infra.Quantity;
 using Abc.Soft;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Soft
 {
