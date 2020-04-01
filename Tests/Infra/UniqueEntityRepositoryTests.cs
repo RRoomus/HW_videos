be7ad25223b72bda6f131e6e016f8099bce01006
Reia <reroom@ttu.ee>
2020-04-01 16:57:20 +0300

    50 video lõpp

---
+++ Tests/Infra/UniqueEntityRepositoryTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading.Tasks;
+using Abc.Aids;
+using Abc.Data.Quantity;
+using Abc.Domain.Quantity;
+using Abc.Infra;
+using Abc.Infra.Quantity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Abc.Tests.Infra
+{
+    [TestClass]
+    public class UniqueEntityRepositoryTests
+    {
+        private MeasureData data;
+
+        private class testClass : UniqueEntityRepository<Measure, MeasureData>
+        {
+            public testClass(DbContext c, DbSet<MeasureData> s) : base(c, s) { }
+
+            protected internal override Measure toDomainObject(MeasureData d) => new Measure(d);
+
+            protected override async Task<MeasureData> getData(string id)
+            {
+                return await dbSet.FirstOrDefaultAsync(m => m.Id == id);
+            }
+
+            protected override string getId(Measure entity) => entity?.Data?.Id;
+        }
+
+        [TestInitialize]
+        public override void TestInitialize()
+        {
+            base.TestInitialize();
+            var options = new DbContextOptionsBuilder<QuantityDbContext>()
+                .UseInMemoryDatabase("TestDb")
+                .Options;
+            var c = new QuantityDbContext(options);
+            obj = new testClass(c, c.Measures);
+            data = GetRandom.Object<MeasureData>();
+        }
+    }
+}
