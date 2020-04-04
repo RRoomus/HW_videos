d447df72c6bcc73c105582fe785b7985fe2b752b
Reia <reroom@ttu.ee>
2020-04-04 13:50:34 +0300

    53 video lõpp

--- Infra/Quantity/QuantityDbContext.cs
+++ Infra/Quantity/QuantityDbContext.cs
@@ -13,7 +13,8 @@ namespace Abc.Infra.Quantity
         public DbSet<UnitData> Units { get; set; }
         public DbSet<SystemOfUnitsData> SystemsOfUnits { get; set; }
         public DbSet<UnitFactorData> UnitFactors { get; set; }
-
+        public DbSet<MeasureTermData> MeasureTerms { get; set; }
+        public DbSet<UnitTermData> UnitTerms { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
@@ -25,8 +26,9 @@ namespace Abc.Infra.Quantity
             builder.Entity<MeasureData>().ToTable(nameof(Measures));
             builder.Entity<UnitData>().ToTable(nameof(Units));
             builder.Entity<SystemOfUnitsData>().ToTable(nameof(SystemsOfUnits));
-            builder.Entity<UnitFactorData>().ToTable(nameof(UnitFactors))
-                .HasKey(x=>new{x.UnitId, x.SystemOfUnitsId});
+            builder.Entity<UnitFactorData>().ToTable(nameof(UnitFactors)).HasKey(x=>new{x.UnitId, x.SystemOfUnitsId});
+            builder.Entity<MeasureTermData>().ToTable(nameof(MeasureTerms)).HasKey(x => new { x.MasterId, x.TermId });
+            builder.Entity<UnitTermData>().ToTable(nameof(UnitTerms)).HasKey(x => new { x.MasterId, x.TermId });
         }
     }
 }
